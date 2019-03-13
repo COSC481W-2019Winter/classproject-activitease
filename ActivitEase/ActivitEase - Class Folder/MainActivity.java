bc27778563fefc614c8daa37bdb92bcf7bed69de
Abean4 <46505859+Abean4@users.noreply.github.com>
2019-03-13 16:57:11 -0400

    Update MainActivity.java

--- ActivitEase/ActivitEase - Class Folder/MainActivity.java
+++ ActivitEase/ActivitEase - Class Folder/MainActivity.java
@@ -16,13 +16,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
 {
     EditText interestName, periodFrequency, basePeriodSpan, activityLength, numNotifications;
-    interest interest = new interest();
+    Interest interest = new Interest();
     String startStopTimerText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +30,9 @@ public class MainActivity extends AppCompatActivity
         FragmentTransaction hp = getSupportFragmentManager().beginTransaction();
         hp.replace(R.id.fragment_container, new Home_Page_Fragment());
         hp.commit();
+
+
+        User db = new User(this);
 
 
 
@@ -76,20 +78,6 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -103,11 +91,7 @@ public class MainActivity extends AppCompatActivity
         else if (id == R.id.FAQ) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FAQ_Fragment()).commit();
 
-        } else if (id == R.id.addInterest) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Add_Interest_Fragment()).commit();
-
         } else if (id == R.id.Interest) {
-
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Interest_Fragment()).commit();
         }
 
@@ -121,17 +105,28 @@ public class MainActivity extends AppCompatActivity
         hp.replace(R.id.fragment_container, new Add_Interest_Fragment());
         hp.commit();
     }
+    /*
     public void submitEditInterest(View view)
     {
         //Throw error if in invalid format
         //Call instance of user to store data
         //From user call function to store data
         //Then return next page with updated content
-        interestName = findViewById(R.id.interestName);
+        interestName = findViewById(R.id.editText);
         String theInterestName = interestName.getText().toString();
         interest.setInterestName(theInterestName);
+        periodFrequency = findViewById(R.id.periodFreq);
+        String periodFreq = periodFrequency.getText().toString();
+        interest.setPeriodFreq(periodFreq);
+        activityLength = findViewById(R.id.activityLength);
+        String theActivityLength = activityLength.getText().toString();
+        interest.setActivityLength(theActivityLength);
+        basePeriodSpan = findViewById(R.id.basePeriodSpan);
+        String thePeriodSpan = basePeriodSpan.getText().toString();
+        interest.setBasePeriodSpan(thePeriodSpan);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Interest_Fragment()).commit();
     }
+    */
     public void setButtonText(String buttonText)
     {
         Button b = findViewById(R.id.startStop);
@@ -164,4 +159,30 @@ public class MainActivity extends AppCompatActivity
         }
 
     }
+
+    public void addInterest(View view){
+
+        User db = new User(this);
+        //EditText interestName = findViewById(R.id.interestName);
+//        String intName= interestName.getText().toString();
+//        int periodFreq = Integer.parseInt(periodFrequency.getText().toString());
+//        int periodSpan = Integer.parseInt(basePeriodSpan.getText().toString());
+//        int length = Integer.parseInt(activityLength.getText().toString());
+//        int numNot = Integer.parseInt(numNotifications.getText().toString());
+
+
+        String intName = "Hello";
+        int periodFreq = 3;
+        int periodSpan = 2;
+        int length = 1;
+        int numNot = 4;
+        interest = new Interest(intName, periodFreq, periodSpan, length, numNot);
+
+
+
+        db.addInterest(interest);
+
+
+    }
+
 }
