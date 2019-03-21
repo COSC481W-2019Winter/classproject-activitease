2b2770975c0e043a403c7872bc50d471337a3c45
Matthew Coon <46505830+mattc8818@users.noreply.github.com>
2019-03-20 20:18:15 -0400

    Add files via upload

--- ActivitEase/ActivitEase - Class Folder/MyDao.java
+++ ActivitEase/ActivitEase - Class Folder/MyDao.java
@@ -4,11 +4,13 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
+import java.util.List;
+
 @Dao
 public interface MyDao {
     @Insert
     void addInterest(Interest i);
 
     @Query("select * from interests")
-    Interest getInterests();
+    List<Interest> getInterests();
 }
