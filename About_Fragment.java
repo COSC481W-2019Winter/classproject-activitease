e00d69a2947b3e015ffd48682433d23338d12e22
eriksali <43148041+eriksali@users.noreply.github.com>
2019-03-20 20:27:45 -0400

    Add files via upload

---
+++ About_Fragment.java
@@ -0,0 +1,16 @@
+
+package com.example.activitease;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class About_Fragment {
+
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.about_page, container, false);
+        }
+    }
