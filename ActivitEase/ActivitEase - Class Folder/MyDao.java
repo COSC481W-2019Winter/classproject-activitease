6c908daef3255d9fe7de4deaab3a18e269be0e69
Abean4 <46505859+Abean4@users.noreply.github.com>
2019-03-26 19:27:54 -0400

    Updated with Search and Update Functions
    loadInterestByName searches the database for the Interest Name in question and loads it. updateInterest updates the interest at hand.

--- ActivitEase/ActivitEase - Class Folder/MyDao.java
+++ ActivitEase/ActivitEase - Class Folder/MyDao.java
@@ -3,14 +3,22 @@ package com.example.activitease;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 
 @Dao
-public interface MyDao {
+interface MyDao {
     @Insert
-    void addInterest(Interest i);
+    public void addInterest(Interest i);
 
     @Query("select * from interests")
     List<Interest> getInterests();
+
+    @Query("select * from interests where interestName = :interestName")
+    Interest loadInterestByName(String interestName);
+
+    @Update
+    public void updateInterest(Interest i);
+
 }
