b8544f6ce1a3f576a6a1f2b5b6b0a8e3ac272e47
Benjamin Onwenu <45779417+bonwenu@users.noreply.github.com>
2019-03-28 13:22:38 -0400

    Fixed crash

--- ActivitEase/ActivitEase - Class Folder/Notification_receiver.java
+++ ActivitEase/ActivitEase - Class Folder/Notification_receiver.java
@@ -1,44 +1,57 @@
 package com.example.activiteasev5;
-
-
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.support.v4.app.NotificationCompat;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
-public class Notification_receiver extends BroadcastReceiver {
-    public void onReceive(Context context, Intent intent) {
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+import static java.lang.String.*;
+
+
+public class Home_Page_Fragment extends Fragment {
+    TextView populateInterests;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_main, container, false);
+
+        populateInterests = view.findViewById(R.id.testDBPopulate);
+
+
+        TextView populateInterests = view.findViewById(R.id.testDBPopulate);
         List<Interest> interestList = MainActivity.myDB.myDao().getInterests();
-        Intent repeating_intent = new Intent(context, MainActivity.class);
-        repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        int numOfInterest = interestList.size();
+        String info = "  "; // Activity Length:    Period Frequency:   base p. span:   notifications: times for notifs: \n";
 
-        for (int i = 0; i < numOfInterest; i++) {
-            System.out.println(interestList.get(i).getInterestName());
+
+        for(Interest intr : interestList) {
+            if (intr != null) {
+                String interestName = intr.getInterestName();
+                int activityLength = intr.getActivityLength();
+                int periodFreq = intr.getPeriodFreq();
+                int basePeriodSpan = intr.getBasePeriodSpan();
+                int numNotifications = intr.getNumNotifications();
+
+                double[] tempNotifTimes = intr.getNotifTimes();
+
+                info += "Interest 1: " +interestName;
+
+                for (int i = 0; i < numNotifications; i++) {
+                    // info += format("%.2f", tempNotifTimes[i]) + "   ";
+                }
+                info += "\n";
+            }
+            else
+                break;
         }
+        populateInterests.setText(info);
 
-        String firstInterestName = interestList.get(0).getInterestName();
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        int icon = R.mipmap.ic_launcher;
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setContentIntent(pendingIntent)
-                //.setSmallIcon(android.R.drawable.arrow_up_float)setSmallIcon(icon);
-                .setSmallIcon(icon)
-                .setContentTitle(firstInterestName)
-                .setContentText("Number of Notification")
-                .setWhen(System.currentTimeMillis())
-                .setAutoCancel(true);
-
-        notificationManager.notify(100, builder.build());
+        return view;
     }
-
 }
