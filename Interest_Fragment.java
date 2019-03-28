2cfb0e06dc10de27346334b039cbea7e7a66fe2b
drewscotton <46579282+drewscotton@users.noreply.github.com>
2019-03-27 20:34:46 -0400

    Add files via upload

--- Interest_Fragment.java
+++ Interest_Fragment.java
@@ -1,17 +1,36 @@
 package com.example.activitease;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.Locale;
+import android.widget.EditText;
+import android.os.AsyncTask;
 
 public class Interest_Fragment extends Fragment {
-     // EditText interestName, periodFrequency, basePeriodSpan, activityLength, numNotifications;
+    // EditText interestName, periodFrequency, basePeriodSpan, activityLength, numNotifications;
+    MyGLSurfaceView glSurfaceView;
+    boolean timerRunning;
+    private static final long START_TIME_MILLIS = 600000;
+    private long mTimeLeftInMillis = START_TIME_MILLIS;
+    private TextView textViewCountdown;
+    private CountDownTimer countDownTimer;
+    private EditText delInterestName;
+    Button delete;
+
 
     @Nullable
     @Override
@@ -19,9 +38,107 @@ public class Interest_Fragment extends Fragment {
     {
         View view = inflater.inflate(R.layout.interest_page, container, false);
         TextView mytextview = view.findViewById(R.id.EditInterestName);
-        interest theInterest = new interest();
+        Interest theInterest = new Interest();
         mytextview.setText(theInterest.getInterestName());
+        String[] periodSpanTypes =
+                {"Day", "Week", "Month", "Year"};
+
+        Spinner periodSpanSpinner = (Spinner) view.findViewById(R.id.periodSpanInput);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, periodSpanTypes);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        periodSpanSpinner.setAdapter(adapter);
+
+        Spinner notificationSpan = (Spinner) view.findViewById(R.id.numNotificationSpan);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, periodSpanTypes);
+        adapter1.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        notificationSpan.setAdapter(adapter1);
+        glSurfaceView = view.findViewById(R.id.openGLView);
+
+        textViewCountdown = view.findViewById(R.id.text_view_countdown);
+        updateCountDownText();
+        if(true)
+        {
+            startTimer();
+        }
+
+        //Stuff past here is for deleting an interest
+        delete=(Button)view.findViewById(R.id.delete);
+
+
+        //final Interest delInterest = MainActivity.myDB.myDao().loadInterestByName(delInterestName1);
+        delInterestName = view.findViewById(R.id.interestName);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String delInterestName1 = delInterestName.getText().toString();
+                AsyncTask.execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        AsyncTask.execute(new Runnable() {
+                            @Override
+                            public void run() {
+                                // and deleting
+                                MainActivity.myDB.myDao().deleteByInterestName(delInterestName1);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+
 
         return view;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        glSurfaceView.onResume();
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        glSurfaceView.onPause();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) mTimeLeftInMillis / 1000 / 60;
+        int seconds = (int) mTimeLeftInMillis / 1000 % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        textViewCountdown.setText(timeLeftFormatted);
+
+    }
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timerRunning = false;
+
+            }
+        }.start();
+
+        timerRunning = true;
+
+    }
+    private void pauseTimer()
+    {
+        countDownTimer.cancel();
+        timerRunning = false;
+
+    }
+    private void resetTimer()
+    {
+        mTimeLeftInMillis = START_TIME_MILLIS;
+        updateCountDownText();
+    }
+
+
 }
