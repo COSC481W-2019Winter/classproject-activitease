2c8de8bb384fc54e0e774ccb0127722581a0fdc6
Matthew Coon <46505830+mattc8818@users.noreply.github.com>
2019-03-28 15:16:19 -0400

    Update Interest_Fragment.java
    -AB

--- ActivitEase/ActivitEase - Class Folder/Interest_Fragment.java
+++ ActivitEase/ActivitEase - Class Folder/Interest_Fragment.java
@@ -32,7 +32,7 @@ public class Interest_Fragment extends Fragment {
     private CountDownTimer countDownTimer;
     private EditText delInterestName;
 
-    Button delete;
+    Button delete, editInterestBn;
 
     private EditText interestName, activityLength, numNotifications, periodFreq;
     private Spinner periodSpanInput;
@@ -47,7 +47,7 @@ public class Interest_Fragment extends Fragment {
     {
         View view = inflater.inflate(R.layout.interest_page, container, false);
         TextView mytextview = view.findViewById(R.id.EditInterestName);
-        Interest theInterest = new Interest();
+        final Interest theInterest = new Interest();
         mytextview.setText(theInterest.getInterestName());
         String[] periodSpanTypes =
                 {"Day", "Week", "Month", "Year"};
@@ -76,14 +76,104 @@ public class Interest_Fragment extends Fragment {
         periodFreq.setText(Integer.toString(pFreq));
 
         glSurfaceView = view.findViewById(R.id.openGLView);
-
+        GLRenderer.setActivityLength(START_TIME_MILLIS);
         textViewCountdown = view.findViewById(R.id.text_view_countdown);
         updateCountDownText();
         if(true)
         {
-            //GLRenderer.setTimerRunning(Interest.getTimerRunning());
+            GLRenderer.setTimerRunning(true);
             startTimer();
         }
+
+
+
+
+//The following lines are for Editing the Interest.
+        interestName = view.findViewById(R.id.interestName);
+        activityLength = view.findViewById(R.id.activityLength);
+        periodFreq = view.findViewById(R.id.periodFreq);
+        periodSpanInput = view.findViewById(R.id.periodSpanInput);
+        numNotifications = view.findViewById(R.id.numNotifications);
+
+        // Finds the submit button, and an onClick method submits the data into the database.
+        editInterestBn = view.findViewById(R.id.SubmitEditInterest);
+        editInterestBn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Interest updateInterest = theInterest;
+                // Sets the interestName, which is the key for the database
+                String newInterestName = interestName.getText().toString();
+
+                /*
+                 * Finds the raw values of the EditTexts and the Spinner, and saves them in
+                 * int and String variables.
+                 */
+                String newActivityLengthTemp = activityLength.getText().toString();
+                String newPeriodFreqTemp = periodFreq.getText().toString();
+                String newPeriodSpan = periodSpanInput.getSelectedItem().toString();
+                String newNumNotificationsTemp = numNotifications.getText().toString();
+
+                if (!newInterestName.equals(updateInterest.getInterestName()))
+                    updateInterest.setInterestName(newInterestName);
+                if (!newActivityLengthTemp.equals(updateInterest.getActivityLength()))
+                    updateInterest.setActivityLength(Integer.parseInt(newActivityLengthTemp));
+                if (!newPeriodFreqTemp.equals(updateInterest.getPeriodFreq()))
+                    updateInterest.setPeriodFreq(Integer.parseInt(newPeriodFreqTemp));
+                if (!newNumNotificationsTemp.equals(updateInterest.getNumNotifications())) {
+                    int newNumNotifications = Integer.parseInt(newNumNotificationsTemp);
+                    updateInterest.setNumNotifications(newNumNotifications);
+                    updateInterest.setNotifTimes(Interest.presetNotifTimes(newNumNotifications));
+                }
+
+
+                if (!newPeriodSpan.equals("Day")) {
+
+
+                    int basePeriodSpan = 0;
+
+                    /*
+                     * Temporary values of basePeriodSpan, which will have to be revised for later.
+                     * basePeriodSpan serves as a numeric representation of how long a day, week, month,
+                     * and year are.
+                     */
+                    switch (newPeriodSpan) {
+                        case "Day":
+                            basePeriodSpan = 1;
+                            break;
+                        case "Week":
+                            basePeriodSpan = 7;
+                            break;
+                        case "Month":
+                            basePeriodSpan = 30;
+                            break;
+                        case "Year":
+                            basePeriodSpan = 365;
+                            break;
+                    }
+                    updateInterest.setBasePeriodSpan(basePeriodSpan);
+                }
+
+
+                // The database updates the interest to the interests table.
+                MainActivity.myDB.myDao().updateInterest(updateInterest);
+
+                // Announces that an interest was successfully edited.
+                Toast.makeText(getActivity(), "Interest edited successfully", Toast.LENGTH_LONG).show();
+
+                // Resets the AddInterest form.
+                interestName.setText(theInterest.getInterestName());
+                activityLength.setText(Integer.toString(theInterest.getActivityLength()));
+                periodFreq.setText(Integer.toString(theInterest.getPeriodFreq()));
+                numNotifications.setText(Integer.toString(theInterest.getNumNotifications()));
+
+            }
+        });
+
+
+
+
+
+
 
         //Stuff past here is for deleting an interest
         delete=(Button)view.findViewById(R.id.delete);
