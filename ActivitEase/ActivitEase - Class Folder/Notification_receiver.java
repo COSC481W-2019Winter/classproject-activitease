e83f963be8d2b154507dd0befd1052d034ba12d4
Benjamin Onwenu <45779417+bonwenu@users.noreply.github.com>
2019-03-31 14:45:45 -0400

    Update Notification_receiver.java

--- ActivitEase/ActivitEase - Class Folder/Notification_receiver.java
+++ ActivitEase/ActivitEase - Class Folder/Notification_receiver.java
@@ -1,45 +1,52 @@
 package com.example.activiteasev5;
 
-
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-
 import java.util.List;
 
 public class Notification_receiver extends BroadcastReceiver {
+
+    public final String CHANNEL_ID = "Personal Notification";
+    private final int NOTIFICATION_ID = 100;
+
+
+
+
     public void onReceive(Context context, Intent intent) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         List<Interest> interestList = MainActivity.myDB.myDao().getInterests();
         Intent repeating_intent = new Intent(context, MainActivity.class);
         repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-        /*int numOfInterest = interestList.size();
+        //int numOfInterest = interestList.size();
 
-        for (int i = 0; i < numOfInterest; i++) {
-            System.out.println(interestList.get(i).getInterestName());
-        }
+        //for (int i = 0; i < numOfInterest; i++) {
+            //System.out.println(interestList.get(i).getInterestName());
+       // }
 
-        String firstInterestName = interestList.get(0).getInterestName();
-*/
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 100, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        //String firstInterestName = interestList.get(0).getInterestName();
 
-        int icon = R.mipmap.ic_launcher;
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+        //createNotificationChannel();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setContentIntent(pendingIntent)
                 //.setSmallIcon(android.R.drawable.arrow_up_float)setSmallIcon(icon);
-                .setSmallIcon(icon)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 //.setContentTitle(firstInterestName)
                 .setContentText("Hey, you! It's time to get this done!")
                 .setWhen(System.currentTimeMillis())
-                .setAutoCancel(true);
+                .setAutoCancel(true)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        notificationManager.notify(100, builder.build());
+        notificationManager.notify((NOTIFICATION_ID), builder.build());
+
     }
 
 }
+
 
