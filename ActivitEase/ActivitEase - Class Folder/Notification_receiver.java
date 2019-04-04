eb8767ac35c7d8175bb16300ca1cdd6401a04394
Benjamin Onwenu <45779417+bonwenu@users.noreply.github.com>
2019-04-04 15:57:53 -0400

    Update Notification_receiver.java

--- ActivitEase/ActivitEase - Class Folder/Notification_receiver.java
+++ ActivitEase/ActivitEase - Class Folder/Notification_receiver.java
@@ -6,12 +6,16 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+
+import java.util.Arrays;
 import java.util.List;
 
 public class Notification_receiver extends BroadcastReceiver {
 
     public final String CHANNEL_ID = "Personal Notification";
-    private final int NOTIFICATION_ID = 100;
+    private final int NOTIFICATION_ID = 101;
+    public final String CHANNEL_ID2 = "Personal Notification";
+    public String[] name;
 
 
 
@@ -19,6 +23,7 @@ public class Notification_receiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         List<Interest> interestList = MainActivity.myDB.myDao().getInterests();
+
         Intent repeating_intent = new Intent(context, MainActivity.class);
         repeating_intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
@@ -27,25 +32,53 @@ public class Notification_receiver extends BroadcastReceiver {
         //for (int i = 0; i < numOfInterest; i++) {
             //System.out.println(interestList.get(i).getInterestName());
        // }
+        name = new String[10];
+        Arrays.fill(name, "");
 
-        //String firstInterestName = interestList.get(0).getInterestName();
+        int k = 0;
+        //String firstInterestName =
+        for(Interest intr : interestList) {
+            if (intr != null) {
+                name[k] = intr.getInterestName();
+                int activityLength = intr.getActivityLength();
+                int periodFreq = intr.getPeriodFreq();
+                int basePeriodSpan = intr.getBasePeriodSpan();
+                int numNotifications = intr.getNumNotifications();
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, NOTIFICATION_ID, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                double[] tempNotifTimes = intr.getNotifTimes();
 
-        //createNotificationChannel();
+
+
+                for (int i = 0; i < numNotifications; i++) {
+                    // info += format("%.2f", tempNotifTimes[i]) + "   ";
+                }
+                k++;
+            }
+            else
+                break;
+        }
+
+
+        PendingIntent pendingIntent1 = PendingIntent.getActivity(context, NOTIFICATION_ID, repeating_intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+        // Interest 1
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
-                .setContentIntent(pendingIntent)
+                .setContentIntent(pendingIntent1)
                 //.setSmallIcon(android.R.drawable.arrow_up_float)setSmallIcon(icon);
                 .setSmallIcon(R.mipmap.ic_launcher)
-                //.setContentTitle(firstInterestName)
+                .setContentTitle(name[0])
                 .setContentText("Hey, you! It's time to get this done!")
                 .setWhen(System.currentTimeMillis())
                 .setAutoCancel(true)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        notificationManager.notify((NOTIFICATION_ID), builder.build());
+        if (name[0] != "")
+            notificationManager.notify((NOTIFICATION_ID), builder.build());
+
 
     }
+
 
 }
 
