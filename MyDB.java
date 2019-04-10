b548ea111bda198f7af3bc5a6bbe9597c92d8c76
Abean4 <46505859+Abean4@users.noreply.github.com>
2019-04-10 17:40:36 -0400

    Delete MyDB.java

--- MyDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.activitease;
-
-import android.arch.persistence.room.Database;
-import android.arch.persistence.room.RoomDatabase;
-
-@Database(entities = {Interest.class}, version = 1)
-public abstract class MyDB extends RoomDatabase {
-    public abstract MyDao myDao();
-}
